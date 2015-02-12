3cc5568abea9d045b520a5ce51a9ce0a2d8f6c92
csparpa <csparpa@gmail.com>
2015-02-12 22:34:03 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # minoto-python
-A super-minimal Python client for Minoto API
+A super-minimal Python client for [Minoto](http://minotovideo.com/) API
+
+It should basically allow to:
+
+  * announce a new Minoto video resource
+  * upload video data (eg: MP4) to a Minoto video resource
+  * get info about a Minoto video resource
+  * delete a Minoto video resource
+
+Please refer to the Minoto documentation for API details and resources model.
+
+## Installation ##
+1. Download/clone source
+2. Install dependencies with `pip install -r requirements.txt`
+
+## Usage ##
+```
+from minoto-python import MinotoClient
+
+publisher_id = 'my-pub-id'
+oauth_params = dict(oauth_access_token='xxx', oauth_token_secret='yyy',
+                    oauth_consumer_key='zzz', oauth_consumer_secret='www')
+
+client = MinotoClient(publisher-id, oauth_params, output_format='json')
+
+# announce a video resource
+video_properties = dict(title='title', description='description', thumbnail_uri='thumb_url',
+                        tags='tag1;tag2;tag3')
+resp = client.announce_video_resource(video_properties)
+minoto_id = resp['minoto_id']       # ID of created video resource
+upload_uri = resp['upload_uri']     # URI which you need to upload the clip to
+upload_token = resp['upload_token'] # token to be attached to the clip upload
+
+# upload a video to a video resource
+client.upload_clip_to_video_resource(upload_uri, upload_token, '/path/to/video/file.mp4'):
+
+# get info about a video resource
+json_blob = client.read_video_resource(minoto_id)
+
+# delete a video resource
+client.delete_video_resource(minoto_id):
+```
---
+++ minoto_python.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import random
+import string
+import json
+import requests
+from requests_toolbelt import MultipartEncoder
+
+
+class MinotoClient():
+
+    VIDEOS_ENDPOINT = 'https://api.minoto-video.com/publishers/%s/videos'
+    VIDEO_URL = VIDEOS_ENDPOINT+'/%s'
+
+    def __init__(self, publisher_id, oauth_params, output_format='json'):
+        self.publisher_id = publisher_id
+        self.oauth_access_token = oauth_params['oauth_access_token']
+        self.oauth_token_secret = oauth_params['oauth_token_secret']
+        self.oauth_consumer_key = oauth_params['oauth_consumer_key']
+        self.oauth_consumer_secret = oauth_params['oauth_consumer_secret']
+        self.oauth_version = '1.0'
+        self.output_format = output_format
+
+    @staticmethod
+    def _generate_timestamp():
+        return int(time.time())
+
+    @staticmethod
+    def _generate_nonce(length=9):
+        return ''.join(random.choice(string.ascii_uppercase + \
+                 string.ascii_lowercase + string.digits) for _ in range(length))
+
+    def _oauth_plain_text_params(self):
+        timestamp = MinotoClient._generate_timestamp()
+        nonce = MinotoClient._generate_nonce(9)
+        oauth_signature_method = 'PLAINTEXT'
+        return dict(oauth_consumer_key=self.oauth_consumer_key,
+                    oauth_nonce=nonce,
+                    oauth_signature_method=oauth_signature_method,
+                    oauth_timestamp=timestamp,
+                    oauth_token=self.oauth_access_token,
+                    oauth_version=self.oauth_version,
+                    output_format=self.output_format,
+                    oauth_signature=self.oauth_consumer_secret+'&'+ \
+                        self.oauth_token_secret)
+
+    def announce_video_resource(self, video_properties):
+        announcement_url = self.VIDEOS_ENDPOINT % (self.publisher_id,)
+        params = self._oauth_plain_text_params()
+        resp = requests.post(announcement_url, params=params,
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(video_properties))
+        video = resp.json()
+        if not resp.status_code == 200 or not video['status'] == 'announced':
+            raise Exception('Impossible to announce video - Minoto API status code: %s' % (resp.status_code,))
+        return dict(minoto_id=video['id'], upload_uri=video['upload_uri'], upload_token=video['upload_token'])
+
+    def upload_clip_to_video_resource(self, upload_uri, upload_token, path_to_video):
+        filename = os.path.basename(path_to_video).split('.')[0]
+        m = MultipartEncoder(fields={'UPLOAD_IDENTIFIER': upload_token,
+                                     'video': (filename, open(path_to_video, 'rb'), 'application/octet-stream')})
+        resp = requests.post(upload_uri, data=m, headers={'Content-Type': m.content_type})
+        if not resp.status_code == 200:
+            raise Exception('Impossible to upload video - Minoto API status code: %s' % (resp.status_code,))
+
+    def read_video_resource(self, minoto_id):
+        url = self.VIDEO_URL % (self.publisher_id, minoto_id)
+        params = self._oauth_plain_text_params()
+        resp = requests.get(url, params=params)
+        if not resp.status_code == 200:
+            raise Exception('Error in reading video %s - Minoto API status code: %s' % (minoto_id, resp.status_code,))
+        return resp.json()
+
+    def delete_video_resource(self, minoto_id):
+        url = self.VIDEO_URL % (self.publisher_id, minoto_id)
+        params = self._oauth_plain_text_params()
+        resp = requests.delete(url, params=params)
+        if not resp.status_code == 204:
+            raise Exception('Impossible to remove video %s - Minoto API status code: %s' % (minoto_id, resp.status_code))
