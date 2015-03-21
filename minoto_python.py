b050ff68bef89bfcee413cff9db32d7d10ad20f7
Claudio <csparpa@gmail.com>
2015-03-21 14:27:58 +0100

    Search by video title

--- minoto_python.py
+++ minoto_python.py
@@ -12,6 +12,7 @@ class MinotoClient():
 
     VIDEOS_ENDPOINT = 'https://api.minoto-video.com/publishers/%s/videos'
     VIDEO_URL = VIDEOS_ENDPOINT+'/%s'
+    SEARCH_BY_TITLE_URL = VIDEOS_ENDPOINT+'?search=%s&searchfields=title'
 
     def __init__(self, publisher_id, oauth_params, output_format='json'):
         self.publisher_id = publisher_id
@@ -68,7 +69,9 @@ class MinotoClient():
         url = self.VIDEO_URL % (self.publisher_id, minoto_id)
         params = self._oauth_plain_text_params()
         resp = requests.get(url, params=params)
-        if not resp.status_code == 200:
+        if resp.status_code == 404:  # not found
+            return None
+        if resp.status_code != 200:
             raise Exception('Error in reading video %s - Minoto API status code: %s' % (minoto_id, resp.status_code,))
         return resp.json()
 
@@ -78,3 +81,13 @@ class MinotoClient():
         resp = requests.delete(url, params=params)
         if not resp.status_code == 204:
             raise Exception('Impossible to remove video %s - Minoto API status code: %s' % (minoto_id, resp.status_code))
+
+    def search_video_resource_by_title(self, title):
+        url = self.SEARCH_BY_TITLE_URL % (self.publisher_id, title)
+        params = self._oauth_plain_text_params()
+        resp = requests.get(url, params=params)
+        if resp.status_code == 404:  # not found
+            return None
+        if not resp.status_code == 200:
+            raise Exception('Error in search - Minoto API status code: %s' % (resp.status_code,))
+        return resp.json()
